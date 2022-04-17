ceb052de3335215d54c0fe528d25c61b5878f6b6
Gericom <fnouwt2@gmail.com>
2022-04-17 11:33:16 +0200

    Got rid of weird volatile reads in mobileSystemsGBLogo.c

--- src/mobileSystemsGBLogo.h
+++ src/mobileSystemsGBLogo.h
@@ -2,9 +2,9 @@
 
 typedef struct
 {
-    u16 bldcnt;
-    u16 bldalpha;
-    u16 bldy;
-    u16 dispcnt;
-    u16 bg0cnt;
+    vu16 bldcnt;
+    vu16 bldalpha;
+    vu16 bldy;
+    vu16 dispcnt;
+    vu16 bg0cnt;
 } gbm_state_t;
